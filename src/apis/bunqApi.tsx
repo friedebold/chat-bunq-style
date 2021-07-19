abc24edb706cb28e3f0bc5c1e8eec014ca542871
Daniel <debold.technologies@gmail.com>
2021-07-19 15:38:39 +0200

    change hhtps back to http

--- src/apis/bunqApi.tsx
+++ src/apis/bunqApi.tsx
@@ -1,13 +1,13 @@
 import { ChatUser, User } from "./model"
 
 export const fetchUsers = async () => {
-  return fetch('https://assignment.bunq.com/users')
+  return fetch('http://assignment.bunq.com/users')
     .then(jsonResponse => jsonResponse.json())
     .then((users: User[]) => users)
 }
 
 export const fetchLastSeen = async (conversationId: string, userId: string) => {
-  return fetch(`https://assignment.bunq.com/conversation/${conversationId}/lastseen/${userId}`)
+  return fetch(`http://assignment.bunq.com/conversation/${conversationId}/lastseen/${userId}`)
     .then(jsonResponse => jsonResponse.json())
     .then((response) => response)
     .catch(err => console.log(err))
@@ -17,7 +17,7 @@ export const fetchConversations = async (userId: string) => {
   //It would be better to be subscribed to a stream of conversationUpdates
   //that way no refresh needed if new conversation started by not activeUser
 
-  return fetch(`https://assignment.bunq.com/conversation/user/${userId}`)
+  return fetch(`http://assignment.bunq.com/conversation/user/${userId}`)
     .then(jsonResponse => jsonResponse.json())
     .then((conversations) => conversations)
     .catch(err => console.log(err))
@@ -25,7 +25,7 @@ export const fetchConversations = async (userId: string) => {
 
 export const createPersonalConversation = async (receiverId: string, userId: string,) => {
   const bodyData = receiverId + ',' + userId
-  return fetch('https://assignment.bunq.com/conversation/personal', {
+  return fetch('http://assignment.bunq.com/conversation/personal', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ users: bodyData })
@@ -37,7 +37,7 @@ export const createPersonalConversation = async (receiverId: string, userId: str
 
 export const createGroupConversation = async (receiverIds: string, userId: string, groupName: string) => {
   const bodyData = receiverIds + ',' + userId
-  return fetch('https://assignment.bunq.com/conversation/group', {
+  return fetch('http://assignment.bunq.com/conversation/group', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
@@ -51,7 +51,7 @@ export const createGroupConversation = async (receiverIds: string, userId: strin
 }
 
 export const markConversationAsSeen = async (conversationId: string, userId: string) => {
-  return fetch(`https://assignment.bunq.com/conversation/${conversationId}/seen/${userId}`, {
+  return fetch(`http://assignment.bunq.com/conversation/${conversationId}/seen/${userId}`, {
     method: 'PUT',
   })
     .catch(err => console.log(err))
@@ -65,8 +65,8 @@ const awaitJson = (responses: Response[]) => Promise.all(responses.map(response 
 export const fetchUsersAndLastSeen = async (users: ChatUser[], conversationId: string) => {
   let promises = []
   for (let i = 0; i < users.length; i++) {
-    promises.push(fetch(`https://assignment.bunq.com/user/${users[i].userid}`))
-    promises.push(fetch(`https://assignment.bunq.com/conversation/${conversationId}/lastseen/${users[i].userid}`))
+    promises.push(fetch(`http://assignment.bunq.com/user/${users[i].userid}`))
+    promises.push(fetch(`http://assignment.bunq.com/conversation/${conversationId}/lastseen/${users[i].userid}`))
   }
   return Promise.all(promises)
     .then(awaitJson)
@@ -84,7 +84,7 @@ export const fetchUsersAndLastSeen = async (users: ChatUser[], conversationId: s
 }
 
 export const sendMessage = async (conversationId: string, senderId: string, message: string) => {
-  return fetch(`https://assignment.bunq.com/conversation/${conversationId}/message/send`, {
+  return fetch(`http://assignment.bunq.com/conversation/${conversationId}/message/send`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
@@ -99,14 +99,14 @@ export const sendMessage = async (conversationId: string, senderId: string, mess
 }
 
 export const getLast100Messages = async (conversationId: string, offset: number) => {
-  return fetch(`https://assignment.bunq.com/conversation/${conversationId}/message/limited?limit=100&offset=${offset}`)
+  return fetch(`http://assignment.bunq.com/conversation/${conversationId}/message/limited?limit=100&offset=${offset}`)
     .then(jsonResponse => jsonResponse.json())
     .then(response => response)
     .catch((err: Error) => console.log(err))
 }
 
 export const fetchNewMessages = async (conversationId: string, lastMsgId: string) => {
-  return fetch(`https://assignment.bunq.com/conversation/${conversationId}/new/${lastMsgId}`)
+  return fetch(`http://assignment.bunq.com/conversation/${conversationId}/new/${lastMsgId}`)
     .then(jsonResponse => jsonResponse.json())
     .then(response => response)
     .catch((err: Error) => console.log(err))
